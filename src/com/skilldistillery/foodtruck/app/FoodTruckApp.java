f85d1b85c41a3c4c835aca6d985f1dded86aef75
skaddington <skaddington92@gmail.com>
2023-03-24 17:36:10 -0600

    initial FoodTruck bits

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	private FoodTruck[] fleet;
+
+	public static void main(String[] args) {
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.run();
+
+	}
+	
+	public void run() {
+		
+	}
+//	App will have the scanner
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private String name;
+	
+	
+	
+//	ctors
+	
+	
+//	getters & setters
+	
+	
+//	toString method
+
+}
+// will NOT have the scanner
